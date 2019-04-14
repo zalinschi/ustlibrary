9fe88423507d9a0c248a41dfe0c19fb687c5cf25
zalinschi <viaceslav.zalinschi@gmail.com>
2019-04-14 22:28:24 +0300

    Add Fields ISBN, ISSN, PAGE NUMBER, CHECKBOX

--- includes/custom-fields.php
+++ includes/custom-fields.php
@@ -5,7 +5,63 @@
 
 function cpt_biblioteca_setup_meta_box() {
 	add_meta_box('cf_preview_book','Preview book','display_preview_book_cf','b3c_library', 'normal', 'high' );
-	add_meta_box('cf_color_book_cover','Book Cover Color','display_color_book_cf','b3c_library', 'normal', 'high' );		
+	add_meta_box('cf_color_book_cover','Book Cover Color','display_color_book_cf','b3c_library', 'normal', 'high' );	
+	add_meta_box('cf_details_book','Book Details','display_book_details_cf','b3c_library', 'normal', 'high' );	
+}
+
+function display_book_details_cf( $books ) {
+	$meta = get_post_meta( $books->ID );
+
+	
+	$book_pages = ( isset( $meta['book_pages'][0] ) && '' !== $meta['book_pages'][0] ) ? $meta['book_pages'][0] : '';
+	$book_isbn  = ( isset( $meta['book_isbn'][0] ) && '' !== $meta['book_isbn'][0] ) ? $meta['book_isbn'][0] : '';
+	$book_issn  = ( isset( $meta['book_issn'][0] ) && '' !== $meta['book_issn'][0] ) ? $meta['book_issn'][0] : '';
+
+	$book_print = ( isset( $meta['book_print'][0] ) &&  '1' === $meta['book_print'][0] ) ? 1 : 0;
+	$book_copypaste = ( isset( $meta['book_copypaste'][0] ) &&  '1' === $meta['book_copypaste'][0] ) ? 1 : 0;
+	$book_download = ( isset( $meta['book_download'][0] ) &&  '1' === $meta['book_download'][0] ) ? 1 : 0;
+	?>
+		<table style='border-spacing: 0; width:100%;'>
+			<tr>
+				<td style="width: 33.33%">
+					<label><?php _e('Numărul de pagini','b3c_library'); ?></label>
+					<input  type='number' name='book_pages'  value='<?php echo $book_pages; ?>'  /> 
+				</td>
+
+				<td style="width: 33.33%">
+					<label><?php _e('ISBN','b3c_library') ?></label>
+					<input  type='text' name='book_isbn'  value='<?php echo $book_isbn; ?>'  />
+				</td>
+
+				<td style="width: 33.33%">
+					<label><?php _e('ISSN','b3c_library') ?></label>
+					<input  type='text' name='book_issn'  value='<?php echo $book_issn; ?>'  />
+				</td>
+			</tr>
+			<tr>
+				<td style="width: 33.33%">
+					<label>
+						<input type="checkbox" name="book_copypaste" value="1" <?php checked( $book_copypaste, 1 ); ?> />
+						<?php esc_attr_e( 'Copy Paste Disable ?', 'b3c_library' ); ?>
+					</label>
+				</td>
+
+				<td style="width: 33.33%">
+					<label>
+						<input type="checkbox" name="book_download" value="1" <?php checked( $book_download, 1 ); ?> />
+						<?php esc_attr_e( 'Disable Download ?', 'b3c_library' ); ?>
+					</label>
+				</td>
+
+				<td style="width: 33.33%"> 
+					<label>
+						<input type="checkbox" name="book_print" value="1" <?php checked( $book_print, 1 ); ?> />
+						<?php esc_attr_e( 'Disable Print ?', 'b3c_library' ); ?>
+					</label>
+				</td>
+			</tr>
+		</table>
+	<?php
 }
 
 function display_color_book_cf( $books ) {
@@ -74,6 +130,29 @@ function ustlib_save_metabox( $post_id,$cpt_name) {
         // Salvăm cîmpurile în BD
 		if ( isset( $_POST['book_pdf_link'] )) { update_post_meta( $post_id, 'book_pdf_link', $_POST['book_pdf_link'] ); }
 		if ( isset( $_POST['book_cover_color'] )) { update_post_meta( $post_id, 'book_cover_color', $_POST['book_cover_color'] ); }
+
+		if ( isset( $_POST['book_pages'] )) { 
+			update_post_meta( $post_id, 'book_pages', $_POST['book_pages'] ); 
+		}
+
+		if ( isset( $_POST['book_isbn'] )) { 
+			update_post_meta( $post_id, 'book_isbn', $_POST['book_isbn'] ); 
+		}
+
+		if ( isset( $_POST['book_issn'] )) { 
+			update_post_meta( $post_id, 'book_issn', $_POST['book_issn'] ); 
+		}
+
+
+		$book_print = ( isset( $_POST['book_print'] ) && '1' === $_POST['book_print'] ) ? 1 : 0;
+		update_post_meta( $post_id, 'book_print', esc_attr( $book_print ) );
+
+		$book_copypaste = ( isset( $_POST['book_copypaste'] ) && '1' === $_POST['book_copypaste'] ) ? 1 : 0;
+		update_post_meta( $post_id, 'book_copypaste', esc_attr( $book_print ) );
+
+		$book_download = ( isset( $_POST['book_download'] ) && '1' === $_POST['book_download'] ) ? 1 : 0;
+		update_post_meta( $post_id, 'book_download', esc_attr( $book_print ) );
+
     }
 }
 
