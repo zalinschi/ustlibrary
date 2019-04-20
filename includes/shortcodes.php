ac08b9f6486e95770eabbb11de9482024cde1779
zalinschi <viaceslav.zalinschi@gmail.com>
2019-04-20 15:35:50 +0300

    Pagination 2

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -68,14 +68,49 @@ function b4uloop($atts) {
 
 <?php 
 }
-add_shortcode("library-loop", "b4uloop");
 
 
 
 
 
+function b4uloop2($atts) {
+
+$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+
+$args = array(
+    'post_type'=>'b3c_library', // Your post type name
+    'posts_per_page' => 6,
+    'paged' => $paged,
+);
+
+$loop = new WP_Query( $args );
+if ( $loop->have_posts() ) {
+    while ( $loop->have_posts() ) : $loop->the_post();
+
+             include( plugin_dir . '/templates/loop-books.php');
+
+    endwhile;
+
+    $total_pages = $loop->max_num_pages;
+
+    if ($total_pages > 1){
+
+        $current_page = max(1, get_query_var('paged'));
+
+        echo paginate_links(array(
+            'base' => get_pagenum_link(1) . '%_%',
+            'format' => '/page/%#%',
+            'current' => $current_page,
+            'total' => $total_pages,
+            'prev_text'    => __('« prev'),
+            'next_text'    => __('next »'),
+        ));
+    }    
+}
+wp_reset_postdata();
+
+}
 
 
-
-    
+add_shortcode("library-loop", "b4uloop2");
  
